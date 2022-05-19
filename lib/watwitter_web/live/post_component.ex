6493b4efbe825ded00e933a18f2fe40f43e837eb
marc@roosoft.com <marc@roosoft.com>
2022-05-19 15:03:38 -0400

    test user can see a list of posts

---
+++ lib/watwitter_web/live/post_component.ex
@@ -0,0 +1,11 @@
+defmodule WatwitterWeb.PostComponent do
+  use WatwitterWeb, :live_component
+
+  def render(assigns) do
+    ~L"""
+    <div id="post-<%= @post.id %>">
+
+    </div>
+    """
+  end
+end
